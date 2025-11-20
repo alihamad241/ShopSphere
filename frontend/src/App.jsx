c3d03b60a3a0cb287c8c3b3947be0fe9d19adf4c
Smook-e <ahmednaser2382005@gmail.com>
2025-11-20 13:51:07 +0200

    added faq

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage.jsx'
 import Contact from './pages/Contact.jsx'
+import Faq from './pages/Faq.jsx'
 
 function App() {
   
 
   return (
     <>
-      <Contact />
+      
+      <Faq />
     </>
   )
 }
