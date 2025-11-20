4331efe72d7654a88832eb94cfabc51b04b9d3bb
kithsope <malakhassanabdelhamid@gmail.com>
2025-11-20 18:47:53 +0200

    cart totals component done, also needs backend,, note: responsiveness not added yet, will add when polishing off

---
+++ frontend/src/components/CartTotals.jsx
@@ -0,0 +1,79 @@
+import { motion } from "framer-motion";
+import { useCartStore } from "../stores/useCartStore";
+import { Link } from "react-router-dom";
+import { MoveRight } from "lucide-react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "../libs/axios";
+
+const stripePromise = loadStripe(
+	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+);
+
+const CartTotals = () => {
+	const { total, subtotal, coupon, isCouponApplied, cart } = useCartStore();
+
+	const savings = subtotal - total;
+	const formattedSubtotal = subtotal.toFixed(2);
+	const formattedTotal = total.toFixed(2);
+	const formattedSavings = savings.toFixed(2);
+
+	const handlePayment = async () => {
+		const stripe = await stripePromise;
+		const res = await axios.post("/payments/create-checkout-session", {
+			products: cart,
+			couponCode: coupon ? coupon.code : null,
+		});
+
+		const session = res.data;
+		const result = await stripe.redirectToCheckout({
+			sessionId: session.id,
+		});
+
+		if (result.error) {
+			console.error("Error:", result.error);
+		}
+	};
+
+  return (
+    <div className="max-w-md mx-auto border shadow-md bg-white">
+      {/* Header */}
+      <div className="bg-black text-white px-4 py-2 font-bold">
+        CART TOTALS
+      </div>
+
+      <div className="p-4 space-y-3">
+        {/* Subtotal */}
+        <div className="flex justify-between text-gray-700">
+          <span className="font-bold">Subtotal</span>
+          <span>${formattedSubtotal}</span>
+        </div>
+
+        {/* Shipping */}
+        <div className="flex justify-between text-gray-700">
+            <span className="font-bold">Shipping</span>
+            <span>Flat Rate: ${formattedShipping}</span>
+        </div>
+
+        {/* Right-aligned Calculate Shipping above total */}
+        <p className="text-right text-sm text-red-600 cursor-pointer">
+            Calculate shipping
+        </p>
+
+        {/* Total */}
+        <div className="flex justify-between border-t border-gray-300 pt-2 text-gray-900">
+            <span className="font-bold">Total</span>
+            <span>${formattedTotal}</span>
+        </div>
+
+        {/* Checkout button */}
+        <div className="text-right">
+        <button className="px-4 inline-block bg-[#00BBA6] text-white py-2 font-bold hover:bg-red-500 cursor-pointer">
+          PROCEED TO CHECKOUT
+        </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartTotals;
