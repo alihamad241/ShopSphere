fe66126dcbd8c2ff1cc677b949bf6b915d11da41
kithsope <malakhassanabdelhamid@gmail.com>
2025-11-21 14:10:22 +0200

    didnt use main ver 4 some reason, nw its using it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,116 @@
-import React from "react";
-import GiftCouponCard from "./components/GiftCouponCard";
-import CartTotals from "./components/CartTotals";
-import CartPage from "./pages/CartPage";
-import { BrowserRouter } from "react-router-dom";
-import TestWishlistItem from "./pages/test";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index2 from "./pages/Index2.jsx";
+import About from "./pages/About.jsx";
+import SingleProduct from "./pages/SingleProduct.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Login from "./pages/Login.jsx";
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Contact from "./pages/Contact.jsx";
+import Faq from "./pages/Faq.jsx";
+import MyAccount from "./pages/MyAccount.jsx";
+import ShopFullwidth from "./pages/ShopFullwidth.jsx";
+import ShopList from "./pages/ShopList.jsx";
+import ShopSidebar from "./pages/ShopSidebar.jsx";
+import ShopPage from "./pages/ShopPage.jsx";
+import Wishlist from "./pages/Wishlist.jsx";
+import Blog from "./pages/Blog.jsx";
+import BlogDetails from "./pages/BlogDetails.jsx";
+import BlogFullwidth from "./pages/BlogFullwidth.jsx";
+import BlogSidebar from "./pages/BlogSidebar.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <TestWishlistItem />
-    </BrowserRouter>
-  );
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Index2 />}
+                />
+                <Route
+                    path="/about"
+                    element={<About />}
+                />
+                <Route
+                    path="/product"
+                    element={<SingleProduct />}
+                />
+                <Route
+                    path="/cart"
+                    element={<Cart />}
+                />
+                <Route
+                    path="/checkout"
+                    element={<Checkout />}
+                />
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                />
+                <Route
+                    path="/faq"
+                    element={<Faq />}
+                />
+                <Route
+                    path="/my-account"
+                    element={<MyAccount />}
+                />
+                <Route
+                    path="/shop"
+                    element={<ShopPage />}
+                />
+                <Route
+                    path="/shop/fullwidth"
+                    element={<ShopFullwidth />}
+                />
+                <Route
+                    path="/shop/list"
+                    element={<ShopList />}
+                />
+                <Route
+                    path="/shop/sidebar"
+                    element={<ShopSidebar />}
+                />
+                <Route
+                    path="/wishlist"
+                    element={<Wishlist />}
+                />
+                <Route
+                    path="/blog"
+                    element={<Blog />}
+                />
+                <Route
+                    path="/blog/details"
+                    element={<BlogDetails />}
+                />
+                <Route
+                    path="/blog/fullwidth"
+                    element={<BlogFullwidth />}
+                />
+                <Route
+                    path="/blog/sidebar"
+                    element={<BlogSidebar />}
+                />
+                <Route
+                    path="/portfolio"
+                    element={<Portfolio />}
+                />
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+                <Route
+                    path="/404"
+                    element={<NotFound />}
+                />
+                <Route
+                    path="*"
+                    element={<NotFound />}
+                />
+            </Routes>
+        </Router>
+    );
 }
+
+export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+            },
+        },
+    },
+});
