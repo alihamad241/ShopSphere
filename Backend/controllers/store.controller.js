d33e7a209c29c0df88c299e405c9de2e1eaba9dd
Karim Abogharbia <karim91_@hotmail.com>
2025-11-24 19:13:17 +0200

    Implement store routes and controller; add store management functionality

---
+++ Backend/controllers/store.controller.js
@@ -0,0 +1,159 @@
+import { redis } from '../libs/redis.js';
+import Product from '../models/product.model.js';
+import Store from '../models/store.model.js';
+import cloudinary from './../libs/cloudinary.js';
+
+export const getAllStores = async (req, res) => {
+    try {
+        const stores = await Store.find({});
+        res.status(200).json({ stores });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching products', error });
+    }
+};
+
+export const getFeaturedStores = async (req, res) => {
+    try {
+        const featuredStores = await redis.get('featured_stores');
+        if (featuredStores) {
+            return res.status(200).json(JSON.parse(featuredStores));
+        }
+
+        featuredStores = await Store.find({ isFeatured: true }).lean();
+
+        if (!featuredStores || featuredStores.length === 0) {
+            return res
+                .status(404)
+                .json({ message: 'No featured stores found' });
+        }
+
+        await redis.set('featured_stores', JSON.stringify(featuredStores)); // Cache for 1 hour
+        res.json(featuredStores);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error fetching featured stores',
+            error
+        });
+    }
+};
+
+export const createStore = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+
+        let cloudinaryResponse = null;
+
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {
+                folder: 'stores'
+            });
+        }
+
+        const store = await Store.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url
+                ? cloudinaryResponse.secure_url
+                : '',
+            category
+        });
+
+        res.status(201).json( store );
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating store', error });
+    }
+};
+
+export const deleteStore = async (req, res) => {
+    try {
+        const store = await Store.findById(req.params.id);
+
+        if (!store) {
+            return res.status(404).json({ message: 'Store not found' });
+        }
+
+        if (store.image) {
+            const publicId = store.image.split('/').pop().split('.')[0];
+            try {
+                await cloudinary.uploader.destroy(`stores/${publicId}`);
+                console.log('Image deleted from Cloudinary');
+            } catch (error) {
+                console.error('Error deleting image from Cloudinary:', error);
+            }
+        }
+
+        await Store.findByIdAndDelete(req.params.id);
+
+        res.json({ message: 'Store deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting store', error });
+    }
+};
+
+export const getRecommendedStores = async (req, res) => {
+    try {
+        const stores = await Store.aggregate([
+            {
+                $sample: { size: 10 }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    image: 1,
+                    price: 1
+                }
+            }
+        ]);
+
+        res.json(stores);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error fetching recommended stores',
+            error
+        });
+    }
+};
+
+export const getStoresByCategory = async (req, res) => {
+    const { category } = req.params;
+    try {
+        const stores = await Store.find({ category: category });
+
+        res.json( {stores} );
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error fetching stores by category',
+            error
+        });
+    }
+};
+
+export const toggleFeaturedStore = async (req, res) => {
+    try {
+        const store = await Store.findById(req.params.id);
+        if (store) {
+            store.isFeatured = !store.isFeatured;
+            const updatedStore = await store.save();
+            await updateFeaturedStoresCache();
+            res.json(updatedStore);
+        } else {
+            res.status(404).json({ message: 'Store not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating store', error });
+    }
+};
+
+async function updateFeaturedStoresCache() {
+    try {
+        const featuredStores = await Store.find({
+            isFeatured: true
+        }).lean();
+        await redis.set('featured_stores', JSON.stringify(featuredStores));
+    } catch (error) {
+        console.error('Error updating featured stores cache:', error);
+    }
+}
