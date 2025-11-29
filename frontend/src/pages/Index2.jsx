8f5e91411c6caeeff40556562da441acb63c846c
Ali Ahmed Hamad <es-ali.ahmed2027@alexu.edu.eg>
2025-11-29 02:29:30 +0200

    refactor: Remove unused components and scripts to streamline the project

--- frontend/src/pages/Index2.jsx
+++ frontend/src/pages/Index2.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import Header from "../components/Header";
 import HeroSlider from "../components/HeroSlider";
 import NewProducts from "../components/NewProducts";
-import BannerArea from "../components/BannerArea";
 import FeaturedProducts from "../components/FeaturedProducts";
-import BlogArea from "../components/BlogArea";
 import BrandLogo from "../components/BrandLogo";
 import Footer from "../components/Footer";
-import ProductModal from "../components/ProductModal";
 
 export default function Index2() {
     return (
@@ -16,13 +13,10 @@ export default function Index2() {
             <main>
                 <HeroSlider />
                 <NewProducts />
-                {/* <BannerArea /> */}
                 <FeaturedProducts />
-                {/* <BlogArea /> */}
                 <BrandLogo />
             </main>
             <Footer />
-            <ProductModal />
         </>
     );
 }
